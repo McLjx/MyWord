1385afa3e88075d70dd84969df74c706224614f9
myljx <285922179@qq.com>
2020-12-12 12:02:12 +0800

    Initial commit

---
+++ app/mylibrary/src/main/java/com/ljx/mylibrary/MyMainLibrary.java
@@ -0,0 +1,7 @@
+package com.ljx.mylibrary;
+
+public  class MyMainLibrary {
+    public int getAge(int a,int b){
+        return a+b;
+    }
+}
---
+++ app/src/main/java/com/ljx/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ljx.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ljx.mylibrary.MyMainLibrary;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyMainLibrary myMainLibrary=new MyMainLibrary();
+
+        Log.d("=====",""+ myMainLibrary.getAge(3,9));
+
+    }
+}
